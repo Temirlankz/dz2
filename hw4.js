ec38fbf4d8f7aead9ac87753060887ee2170c735
Алтай Темирлан Елдосулы <AltayT@lcoast.itstep>
2022-09-19 18:46:44 +0600

    first commit

---
+++ hw4.js
@@ -0,0 +1,9 @@
+let year = prompt();
+let num1 = 400;
+let num2 = 4;
+ if (year % num1 == 0 || year % num2 == 0 && year % 100 != 0 ) {
+     alert('year')
+ } 
+ else  {
+     alert('not year')
+ }
---
+++ hw9.js
@@ -0,0 +1,11 @@
+let capital = prompt('What is the capital of Great Britain? 1 - London 2 - New York 3 - Paris');
+let currency = prompt('What is the currency of the USA? 1 - USD 2 - UAH 3 - RMB');
+let country = prompt('What is the most populated country? 1 - the USA 2 - Japan 3 - China');
+let score = 0;
+if (capital == 1)
+    score += 2;
+if (currency == 1)
+    score += 2;
+if (country == 3)
+    score += 2;
+alert('Your score is ' + score);
